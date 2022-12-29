76a37095f07a383dd6ef09cdc4a6227091abe3e3
Sekler <seklertyoma@gmail.com>
2022-12-30 01:57:24 +0300

    feat: implement distinct

--- src/03-arrays-tasks.js
+++ src/03-arrays-tasks.js
@@ -498,8 +498,13 @@ function getIntervalArray(start, end) {
  *   [ 'a', 'a', 'a', 'a' ]  => [ 'a' ]
  *   [ 1, 1, 2, 2, 3, 3, 4, 4] => [ 1, 2, 3, 4]
  */
-function distinct(/* arr */) {
-  throw new Error('Not implemented');
+function distinct(arr) {
+  return arr.reduce((uni, el) => {
+    if (uni.indexOf(el) === -1) {
+      uni.push(el);
+    }
+    return uni;
+  }, []);
 }
 
 /**
