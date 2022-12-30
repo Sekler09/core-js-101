c11cb0b62fa2fd265867c0b8599e91f2905970d7
Sekler <seklertyoma@gmail.com>
2022-12-31 02:48:30 +0300

    feat: implement getDigitalRoot

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -308,8 +308,17 @@ function isCreditCardNumber(ccn) {
  *   10000 ( 1+0+0+0+0 = 1 ) => 1
  *   165536 (1+6+5+5+3+6 = 26,  2+6 = 8) => 8
  */
-function getDigitalRoot(/* num */) {
-  throw new Error('Not implemented');
+function getDigitalRoot(num) {
+  let sum = 0;
+  let v = num;
+  while (v) {
+    sum += v % 10;
+    v = Math.floor(v / 10);
+  }
+  while (sum >= 10) {
+    sum = getDigitalRoot(sum);
+  }
+  return sum;
 }
 
 
