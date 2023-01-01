ebb9f5d2eb585949f01251e6ee22dbae82556d2c
Sekler <seklertyoma@gmail.com>
2023-01-02 02:09:29 +0300

    feat: implement getPolynom

--- src/08-functions-n-closures-tasks.js
+++ src/08-functions-n-closures-tasks.js
@@ -62,8 +62,15 @@ function getPowerFunction(exponent) {
  *   getPolynom(8)     => y = 8
  *   getPolynom()      => null
  */
-function getPolynom() {
-  throw new Error('Not implemented');
+function getPolynom(...args) {
+  return (n) => {
+    if (!args.length) return null;
+    let res = 0;
+    for (let i = 0; i < args.length; i += 1) {
+      res += args[i] * (n ** (args.length - 1 - i));
+    }
+    return res;
+  };
 }
 
 
