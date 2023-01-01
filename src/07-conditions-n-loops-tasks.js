6777592d39a9f59857b7a00111c786ddda1218b8
Sekler <seklertyoma@gmail.com>
2023-01-02 01:42:13 +0300

    feat: implement getMatrixProduct

--- src/07-conditions-n-loops-tasks.js
+++ src/07-conditions-n-loops-tasks.js
@@ -458,8 +458,19 @@ function getCommonDirectoryPath(pathes) {
  *                         [ 6 ]]
  *
  */
-function getMatrixProduct(/* m1, m2 */) {
-  throw new Error('Not implemented');
+function getMatrixProduct(m1, m2) {
+  const res = new Array(m1.length);
+  for (let i = 0; i < res.length; i += 1) {
+    res[i] = new Array(m2[0].length);
+  }
+  for (let k = 0; k < m2[0].length; k += 1) {
+    for (let i = 0; i < m1.length; i += 1) {
+      let sum = 0;
+      for (let j = 0; j < m2.length; j += 1) sum += m1[i][j] * m2[j][k];
+      res[i][k] = sum;
+    }
+  }
+  return res;
 }
 
 
